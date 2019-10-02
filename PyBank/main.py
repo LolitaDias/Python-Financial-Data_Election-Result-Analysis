9b0161ca7f81a630914c522042c6b5cb76161a4d
Lolita Dias <lolita.dias@gmail.com>
2019-10-02 16:50:33 -0400

    Modified the code for average count

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,51 +8,60 @@ import csv
 csvpath =os.path.join("..","/Users/lolitadias/desktop/PyBank","PyBank_Resources_budget_data.csv")
 #Open csv file
 with open(csvpath, newline="",errors='ignore') as csvfile:
+
     #CSV file specifies the delimeter and variable that holds contents
     csvreader=csv.reader(csvfile,delimiter=',')
+
     # Read The Header Row First (Skip This Step If There Is No Header)
     csv_header = next(csvreader)
+
     #Define variables/lists; Initialize variables
     lmonths =[] # list to store data from column1 of csv file
-    average_change=[] 
-    month_count=[]
-    var1=next(csvreader)
-    tamount=int(var1[1]) #variable to count total amount of "Profit/Losses"(set the initital value to the value in Row1)
-    var2 = int(var1[1]) #variable to store the value at row 1 to calculate revenue change -for the first iteration
+    lamount=[] # list to store data from column1 of csv file
+    vamount=0 #variable to count total amount of "Profit/Losses"(set the initital value to zero)
+    lavg_change=[] # list to store average
     temp_increase=0 #temporary variable to store greatest increase in between iterations
     temp_decrease=0 #temporary variable to store greatest decrease in between iterations
+    vrow=next(csvreader) # increment to the line 2 of csv file, to ignore the header
+    vtemp = int(vrow[1])
+    vamount=int(vrow[1])
+  
     for row in csvreader:
-        #The total number of months included in the dataset
-        lmonths.append(row[0])
-        tmonths=len(lmonths) +1
+            lmonths.append(row[0])
+            lamount.append(row[1])
 
-        #The net total amount of "Profit/Losses" over the entire period
-        tamount=tamount+int(row[1])
+            #The total number of months included in the dataset
+            vmonths=len(lmonths)
 
-        #The average of the changes in "Profit/Losses" over the entire period 
-        revenue_change = int(row[1]) - var2
-        average_change.append(revenue_change)
-        var2 = int(row[1])
-        month_count.append(row[0]) 
-        avg_change = sum(average_change)/ len(average_change)
-        
-        #The greatest increase in profits (date and amount) over the entire period
-        if int(row[1]) > temp_increase:
-            temp_increase=int(row[1])
-            temp_imonth=row[0]
+            #The net total amount of "Profit/Losses" over the entire period 
+            vamount= vamount + int(row[1]) 
+            
+            #The total number of months included in the dataset -Correct code
+            vmonths=len(lmonths) +1
 
-        if int(row[1]) < temp_decrease:
-            temp_decrease=int(row[1])
-            temp_dmonth=row[0]
+            #The average of the changes in "Profit/Losses" over the entire period 
+            tavg_change = int(row[1]) - vtemp
+            lavg_change.append(tavg_change)
+            vtemp= int(row[1])
+            avg_change = sum(lavg_change)/ len(lavg_change)
 
-        high=max(average_change)
-        low=min(average_change)
+            #The greatest increase in profits (date and amount) over the entire period
+            if int(row[1]) > temp_increase:
+                    temp_increase=int(row[1])
+                    temp_imonth=row[0]
+
+            if int(row[1]) < temp_decrease:
+                    temp_decrease=int(row[1])
+                    temp_dmonth=row[0]
+
+            high=max(lavg_change)
+            low=min(lavg_change)
 
     #Print the result
     print('Financial Analysis')
     print('------------------------------------------------------')
-    print(f'Total Months : {tmonths}')
-    print(f'Total : ${tamount}')
+    print(f'Total Months : {vmonths}')
+    print(f'Total : ${vamount}')
     print(f'Average  Change:  ${avg_change:.2f}')
     print(f'Greatest Increase in Profits: {temp_imonth} (${high})')
     print(f'Greatest Decrease in Profits: {temp_dmonth} (${low})')
@@ -64,9 +73,9 @@ file_path=open("Resources_Budget_Output.text","w")
 # Write New Data
 file_path.write(f'Financial Analysis\n')
 file_path.write(f'-----------------------------------------------------\n')
-file_path.write(f'Total Months: {tmonths}\n')
-file_path.write(f'Total: ${tamount}\n')
-file_path.write(f'Average Change: ${avg_change}\n')
+file_path.write(f'Total Months: {vmonths}\n')
+file_path.write(f'Total: ${vamount}\n')
+file_path.write(f'Average Change: ${avg_change:.2f}\n')
 file_path.write(f'Greatest Increase in Profits: {temp_imonth} (${high})\n')
 file_path.write(f'Greatest Decrease in Profits: {temp_dmonth} (${low})')
     
